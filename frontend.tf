b9210215186e29c36a9cb5225bc9758d3ecab35d
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-18 21:46:38 +0530

    terraform first code line

--- frontend.tf
+++ frontend.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-043ff9d2da877c20a"]
 
   tags = {
     Name = "frontend"
