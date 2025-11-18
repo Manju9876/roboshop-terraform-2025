b7bceda5dba0da4fac71dbce12b26a271ff34a37
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-18 22:04:01 +0530

    instance along with dns records

--- catalogue.tf
+++ catalogue.tf
@@ -1,17 +1,17 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-043ff9d2da877c20a"]
 
   tags = {
-    Name = "frontend-dev"
+    Name = "catalogue-dev"
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "catalogue" {
   zone_id = "Z03117651054LFO2TDC32"
-  name    = "frontend-dev"
+  name    = "catalogue-dev"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.catalogue.private_ip]
 }
