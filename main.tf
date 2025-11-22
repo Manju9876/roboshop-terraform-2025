071fb462524c4a265b575485bd33f4dfe1d82565
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-22 10:36:31 +0530

    teraform code

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "instances" {
 }
 
 
-resource "aws_route53_record" "catalogue" {
+resource "aws_route53_record" "record" {
   count = length(var.instance_name)
   zone_id = var.zone_id
   name    = "${var.instance_name[count.index]}-${var.env}"
