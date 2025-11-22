64a99bb9eb90539c682efc8b5603b099bec31004
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-22 14:05:21 +0530

    teraform code

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "ec2" {
 
   ami_id = each.value["ami_id"]
   env    = var.env
-  instance_type = each.value["instance_type "]
+  instance_type = each.value["instance_type"]
   name = each.key
   vpc_security_group-ids = var.vpc_security_group-ids
   zone_id = var.zone_id
