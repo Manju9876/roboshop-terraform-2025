546241366e8699b741439e98db647d24d8f9abaa
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-24 22:03:27 +0530

    teraform code

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_instance" "instances" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
@@ -14,4 +15,21 @@ resource "aws_route53_record" "records" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.instances.private_ip]
+}
+
+resource "null_resource" "ansible_code" {
+  provisioner "remote-exec" {
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      password = "DevOps321"
+      host = aws_instance.instances.private_ip
+    }
+
+    inline = [
+      "sudo python3.11 -m pip install ansible",
+      "ansible-pull -i localhostm -U https://github.com/Manju9876/roboshop-ansible-2025 roboshop.yaml -e component_name=${var.tag_name} -e enc=${var.env}"
+    ]
+  }
 }
