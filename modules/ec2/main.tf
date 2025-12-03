82fc121c3d35c76f3f902c021d16942019926c46
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-12-03 20:46:09 +0530

    teraform code

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -30,7 +30,7 @@ resource "null_resource" "ansible_code" {
 
     inline = [
       "sudo python3.11 -m pip install ansible",
-      "ansible-pull -i localhost, -U https://github.com/Manju9876/roboshop-ansible-2025 roboshop.yaml-e component_name=${var.tag_name} -e env=${var.env} -e vault_token=${var.vault_token}"
+      "ansible-pull -i localhost, -U https://github.com/Manju9876/roboshop-ansible-2025 roboshop.yaml -e component_name=${var.tag_name} -e env=${var.env} -e vault_token=${var.vault_token}"
     ]
   }
 }
