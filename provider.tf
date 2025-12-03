75b0709dcc213b1f74f8bf6091edf426152e524a
Manju <manjunath960650@gmail.com>
2025-12-03 15:14:27 +0530

    terraform practice

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 provider "vault" {
-  address = "http://vault.devopsbymanju.shop:8200"
+  address = "http://3.81.29.104:8200"
   token = var.vault_token
 }
