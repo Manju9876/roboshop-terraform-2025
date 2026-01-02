8186e19338a48acc913605f8a8fc0d7c608a40b4
Manju9876 <manjunathb960650@gmail.com>
2026-01-02 15:16:09 +0530

    terraform practice

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,23 +1,23 @@
-# resource "aws_instance" "instances" {
-#   ami                    = var.ami_id
-#   instance_type          = var.instance_type
-#   vpc_security_group_ids = var.vpc_security_group_ids
-# #  key_name               = var.key_name
-#
-#   # instance_market_options {
-#   #   market_type = "spot"
-#   #
-#   #   spot_options {
-#   #     instance_interruption_behavior = "stop"
-#   #     spot_instance_type = "persistent"
-#   #
-#   #   }
-#   # }
-#
-#   tags = {
-#     Name = var.tag_name
-#   }
-# }
+resource "aws_instance" "instances" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+#  key_name               = var.key_name
+
+  # instance_market_options {
+  #   market_type = "spot"
+  #
+  #   spot_options {
+  #     instance_interruption_behavior = "stop"
+  #     spot_instance_type = "persistent"
+  #
+  #   }
+  # }
+
+  tags = {
+    Name = var.tag_name
+  }
+}
 
 
 resource "aws_route53_record" "records" {
@@ -29,44 +29,44 @@ resource "aws_route53_record" "records" {
 }
 
 
-# resource "null_resource" "ansible_code" {
-#   depends_on = [aws_route53_record.records]
-#
-#   triggers = {
-#     instance_id_change = aws_instance.instances.id
-#   }
-#   provisioner "remote-exec" {
-#
-#     connection {
-#       type     = "ssh"
-#       user     = data.vault_generic_secret.sample.data["username"]
-# #      private_key = file(var.private_key_pem)
-#       password = data.vault_generic_secret.sample.data["password"]
-#       host     = aws_instance.instances.private_ip
-#     }
-#
-#     inline = [
-#       "sudo python3.11 -m pip install ansible hvac",
-#       "ansible-pull -i localhost, -U https://github.com/Manju9876/roboshop-ansible-2025 roboshop.yaml -e component_name=${var.tag_name} -e env=${var.env} -e vault_token=${var.vault_token}"
-#     ]
-#   }
-# }
+resource "null_resource" "ansible_code" {
+  depends_on = [aws_route53_record.records]
 
+  triggers = {
+    instance_id_change = aws_instance.instances.id
+  }
+  provisioner "remote-exec" {
 
-resource "aws_instance" "instances" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group_ids
-  key_name      = var.key_name
+    connection {
+      type     = "ssh"
+      user     = data.vault_generic_secret.sample.data["username"]
+#      private_key = file(var.private_key_pem)
+      password = data.vault_generic_secret.sample.data["password"]
+      host     = aws_instance.instances.private_ip
+    }
 
-  user_data = <<EOF
-#!/bin/bash
-dnf install -y python3.11
-pip3 install ansible hvac
-ansible-pull -U https://github.com/Manju9876/roboshop-ansible-2025 roboshop.yaml \
-  -e component_name=${var.tag_name} \
-  -e env=${var.env} \
-  -e vault_token=${var.vault_token}
-EOF
+    inline = [
+      "sudo python3.11 -m pip install ansible hvac",
+      "ansible-pull -i localhost, -U https://github.com/Manju9876/roboshop-ansible-2025 roboshop.yaml -e component_name=${var.tag_name} -e env=${var.env} -e vault_token=${var.vault_token}"
+    ]
+  }
 }
 
+#
+# resource "aws_instance" "instances" {
+#   ami           = var.ami_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = var.vpc_security_group_ids
+#   key_name      = var.key_name
+#
+#   user_data = <<EOF
+# #!/bin/bash
+# dnf install -y python3.11
+# pip3 install ansible hvac
+# ansible-pull -U https://github.com/Manju9876/roboshop-ansible-2025 roboshop.yaml \
+#   -e component_name=${var.tag_name} \
+#   -e env=${var.env} \
+#   -e vault_token=${var.vault_token}
+# EOF
+# }
+#
