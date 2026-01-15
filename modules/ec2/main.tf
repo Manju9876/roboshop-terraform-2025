2d14929a557cee0005aeaa3539d962bd294d5d3b
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2026-01-15 09:43:53 +0530

    d

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -31,28 +31,28 @@ resource "aws_route53_record" "records" {
 
 
 
-# resource "null_resource" "ansible_code" {
-#   depends_on = [aws_route53_record.records]
-#
-#   triggers = {
-#     instance_id_change = aws_instance.instances.id
-#   }
-#   provisioner "remote-exec" {
-#
-#     connection {
-#       type     = "ssh"
-#       user     = data.vault_generic_secret.sample.data["username"]
-# #      private_key = file(var.private_key_pem)
-#       password = data.vault_generic_secret.sample.data["password"]
-#       host     = aws_instance.instances.private_ip
-#     }
-#
-#     inline = [
-#       "sudo python3.11 -m pip install ansible hvac",
-#       "ansible-pull -i localhost, -U https://github.com/Manju9876/roboshop-ansible-2025 roboshop.yaml -e component_name=${var.tag_name} -e env=${var.env} -e vault_token=${var.vault_token}"
-#     ]
-#   }
-# }
+resource "null_resource" "ansible_code" {
+  depends_on = [aws_route53_record.records]
+
+  triggers = {
+    instance_id_change = aws_instance.instances.id
+  }
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = data.vault_generic_secret.sample.data["username"]
+#      private_key = file(var.private_key_pem)
+      password = data.vault_generic_secret.sample.data["password"]
+      host     = aws_instance.instances.private_ip
+    }
+
+    inline = [
+      "sudo python3.11 -m pip install ansible hvac",
+      "ansible-pull -i localhost, -U https://github.com/Manju9876/roboshop-ansible-2025 roboshop.yaml -e component_name=${var.tag_name} -e env=${var.env} -e vault_token=${var.vault_token}"
+    ]
+  }
+}
 
 # resource "null_resource" "ansible_code" {
 #   depends_on = [aws_route53_record.records]
