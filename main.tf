fc9c35914488e230f0719c66f819888583d4ed2b
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2026-02-11 20:19:39 +0530

    kubernetes learning practice

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-# module "ec2" {
-#   for_each = var.db_instances
-#   source   = "./modules/ec2"
-#
-#   ami_id                 = each.value["ami_id"]
-#   instance_type          = each.value["instance_type"]
-#   vpc_security_group_ids = var.vpc_security_group_ids
-#   zone_id                = var.zone_id
-#   env                    = var.env
-#   tag_name               = each.key
-#   vault_token            = var.vault_token
-#   docker_component_name  = lookup(each.value, "docker_component_name", each.key)
-#
-#  # ansible_role            = lookup(each.value, "ansible_role", each.key)
-# #  key_name               = each.value["key_name"]
-# #  private_key_pem            = var.private_key_pem
-# }
+module "ec2" {
+  for_each = var.db_instances
+  source   = "./modules/ec2"
+
+  ami_id                 = each.value["ami_id"]
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = var.vpc_security_group_ids
+  zone_id                = var.zone_id
+  env                    = var.env
+  tag_name               = each.key
+  vault_token            = var.vault_token
+  docker_component_name  = lookup(each.value, "docker_component_name", each.key)
+
+ # ansible_role            = lookup(each.value, "ansible_role", each.key)
+#  key_name               = each.value["key_name"]
+#  private_key_pem            = var.private_key_pem
+}
 
 module "eks" {
   for_each = var.eks
