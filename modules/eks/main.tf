2d12792c648020a8f37d419f9958c40b09835a8b
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2026-03-08 15:52:05 +0530

    helm deployment

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -55,14 +55,3 @@ resource "null_resource" "kubeconfig" {
   }
 }
 
-resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name   = "vpc-cni"
-
-  configuration_values = jsonencode({
-    env = {
-      ENABLE_PREFIX_DELEGATION = "true"
-      WARM_PREFIX_TARGET       = "1"
-    }
-  })
-}
